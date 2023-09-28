dc592bdd66502d9f09c1a3101bae92a4bc53fbc4
Marcel-MD <marcelvlasenco@gmail.com>
2023-09-28 17:24:56 +0300

    Add redis cache

--- gateway/src/app.module.ts
+++ gateway/src/app.module.ts
@@ -6,11 +6,19 @@ import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
 import { OrderModule } from './order/order.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: process.env.REDIS_HOST || 'redis',
+      port: process.env.REDIS_PORT || 6379,
+    }),
     ThrottlerModule.forRoot([
       {
         ttl: 1000,
@@ -27,6 +35,10 @@ import { APP_GUARD } from '@nestjs/core';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })
